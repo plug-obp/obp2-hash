23bc7f590b79ed56f344f86a2232f7fd47c9cc72
Ciprian Teodorov <ciprian.teodorov@gmail.com>
2021-12-18 22:40:33 +0100

    added a spooky mapping that returns 4 int hashes instead of 2 long hashes

--- src/main/c/obp2_hash_jni.c
+++ src/main/c/obp2_hash_jni.c
@@ -42,6 +42,43 @@ JNIEXPORT jboolean JNICALL Java_obp2_hash_SpookyHashV2_hash128 (
     return 0;
 }
 
+JNIEXPORT jboolean JNICALL Java_obp2_hash_SpookyHashV2_hash128int4 (
+    JNIEnv *env, jobject self, jbyteArray ibuffer, jintArray iseed_ohash) {
+
+    size_t buffer_length = (size_t)((*env)->GetArrayLength(env, ibuffer));
+    if (buffer_length < 1) {
+        //nothing to do
+        return 0;
+    }
+
+    jboolean bufferWasCopied = 0;
+    void *theBuffer = (*env)->GetPrimitiveArrayCritical(env, ibuffer, &bufferWasCopied);
+    if (theBuffer == NULL) {
+        return 1;
+    }
+    jboolean seedsWereCopied = 0;
+    void *theSeeds  = (*env)->GetPrimitiveArrayCritical(env, iseed_ohash, &seedsWereCopied);
+    if (theBuffer == NULL) {
+        return 1;
+    }
+
+    uint64_t *seed1 = theSeeds;
+    uint64_t *seed2 = ((uint64_t *)theSeeds) + 1;
+
+    // printf("seeds in c : (%llu, %llu)\n", *seed1, *seed2);
+    // fflush(stdout);
+
+    spookyhashv2_hash128(theBuffer, buffer_length, seed1, seed2);
+
+    // printf("hashes in c : (%lld, %lld)\n", *seed1, *seed2);
+    // fflush(stdout);
+
+    (*env)->ReleasePrimitiveArrayCritical(env, ibuffer, theBuffer, 0);
+    (*env)->ReleasePrimitiveArrayCritical(env, iseed_ohash, theSeeds, 0);
+
+    return 0;
+}
+
 JNIEXPORT jlong JNICALL Java_obp2_hash_SpookyHashV2_hash64 (
     JNIEnv *env, jobject self, jbyteArray ibuffer, jlong seed) {
 
--- src/main/java/obp2/hash/SpookyHashV2.java
+++ src/main/java/obp2/hash/SpookyHashV2.java
@@ -7,6 +7,8 @@ public class SpookyHashV2 extends ExternalHash {
     //void spookyhashv2_hash128(const void *message,size_t length, uint64 *hash1, uint64 *hash2)
     public static native boolean hash128(byte[] buffer, long[] seedin_hashout);
 
+    public static native boolean hash128int4(byte[] buffer, int[] seedin_hashout);
+
     //uint64_t spookyhashv2_hash64(const void *message,size_t length, uint64_t seed);
     public static native long hash64(byte[] buffer, long seed);
 
@@ -25,6 +27,24 @@ public class SpookyHashV2 extends ExternalHash {
             seeds[1] = rnd.nextLong();
 
             hash128(bytes, seeds);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("spooky2_hash128 " + "("+nb_bytes+", "+ runs+") took " + (endTime - startTime) + " milliseconds");
+    }
+
+    private static void testSpooky128int(int nb_bytes, int runs) {
+        byte[] bytes = new byte[nb_bytes];
+        Random rnd = new Random();
+        int[] seeds = new int[4];
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < runs; i += 2) {
+            rnd.nextBytes(bytes);
+            seeds[0] = rnd.nextInt();
+            seeds[1] = rnd.nextInt();
+            seeds[2] = rnd.nextInt();
+            seeds[3] = rnd.nextInt();
+
+            hash128int4(bytes, seeds);
         }
         long endTime = System.currentTimeMillis();
         System.out.println("spooky2_hash128 " + "("+nb_bytes+", "+ runs+") took " + (endTime - startTime) + " milliseconds");
@@ -62,6 +82,7 @@ public class SpookyHashV2 extends ExternalHash {
         int size = 50000;
         int runs = 10000;
         testSpooky128(size, runs);
+        testSpooky128int(size, runs);
         testSpooky64(size, runs);
         testSpooky32(size, runs);
         testArraysHashCode(size, runs);
